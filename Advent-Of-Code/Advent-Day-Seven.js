b2356f6dff7106ea846ff5bc13f24816bd5cdc88
Nick <nicolanaydenov@gmail.com>
2017-12-07 10:32:18 +0200

    Some cleanup

--- Advent-Of-Code/Advent-Day-Seven.js
+++ Advent-Of-Code/Advent-Day-Seven.js
@@ -1,69 +1,71 @@
 function adventDaySeven(data) {
-  let arrayOfNodes = [];
-  data.forEach((inputLine => {
-    let split = inputLine.split(',');
-    arrayOfNodes[split[0]] = { nodeId:split[0], _root:null, weight:split[1], children:split.slice(2), trueWeight:0 };
-  }));
-  arrayOfNodes = updateNodes(arrayOfNodes);
-  let rootNode = findRoot(arrayOfNodes);
-  console.log(rootNode);
-  calculateNodeWeight(arrayOfNodes, rootNode);
-  findBadNode(arrayOfNodes, rootNode);
+    let arrayOfNodes = [];
+    data.forEach((inputLine => {
+        let split = inputLine.split(',');
+        arrayOfNodes[split[0]] = { nodeId:split[0], _root:null, weight:split[1], children:split.slice(2), trueWeight:0 };
+    }));
+    arrayOfNodes = updateArrayOfNodes(arrayOfNodes);
+    let rootNode = findRoot(arrayOfNodes);
+    console.log("RootNode : " + rootNode.nodeId);
+    calculateNodeWeight(rootNode);
+    findBadNode(rootNode);
 }
-
-function findBadNode(arrayOfNodes, rootNode) {
-  let badNode = findBadWeightNode(arrayOfNodes, rootNode);
-  console.log("bad Node");
-  arrayOfNodes[badNode._root].children.forEach(child => {
-    console.log(arrayOfNodes[child]);
-  });
-  console.log("With Children");
-  badNode.children.forEach(child => {
-    console.log(arrayOfNodes[child]);
-  });
-}
-
-function findBadWeightNode(arrayOfNodes, rootNode) {
-  let badNode = rootNode;
-  if (rootNode.children.length > 0) {
-    let aWeight = findACommonWeight(arrayOfNodes, rootNode);
-    rootNode.children.forEach(child => {
-      if (!(arrayOfNodes[child].trueWeight === aWeight)) {
-        badNode = findBadWeightNode(arrayOfNodes, arrayOfNodes[child]);
-      }
+function findBadNode(rootNode) {
+    let badNode = findBadWeightNode(rootNode);
+    console.log("Bad Node");
+    badNode._root.children.forEach(child => {
+        console.log("{ Node ID : " + child.nodeId);
+        console.log("Weight : " + child.weight);
+        console.log("True Weight : " + child.trueWeight + " }");
     });
-  }
-  return badNode;
-}
-
-function findACommonWeight(arrayOfNodes, rootNode) {
-  let aWeight = arrayOfNodes[rootNode.children[0]].trueWeight;
-  for (let i = 1; i < rootNode.children.length; i++) {
-    if (arrayOfNodes[rootNode.children[i]].trueWeight === aWeight) return aWeight;
-  }
-  return arrayOfNodes[rootNode.children[1]].trueWeight;
-}
-
-function calculateNodeWeight(arrayOfNodes, node) {
-  let weight = +node.weight;
-  node.children.forEach(childNode => {
-    weight += +calculateNodeWeight(arrayOfNodes, arrayOfNodes[childNode]);
-  });
-  node.trueWeight = weight;
-  return weight;
-}
-
-function updateNodes(arrayOfNodes) {
-  for (let key in arrayOfNodes) {
-    arrayOfNodes[key].children.forEach(node => {
-      arrayOfNodes[node]._root = key;
+    console.log("With Children");
+    badNode.children.forEach(child => {
+        console.log("{ Node ID : " + child.nodeId);
+        console.log("Weight : " + child.weight);
+        console.log("True Weight : " + child.trueWeight + " }");
     });
-  }
-  return arrayOfNodes;
+}
+function findBadWeightNode(rootNode) {
+    let badNode = rootNode;
+    if (rootNode.children.length > 0) {
+        let aWeight = findACommonWeight(rootNode);
+        rootNode.children.forEach(child => {
+            if (!(child.trueWeight === aWeight)) {
+                badNode = findBadWeightNode(child);
+            }
+        });
+    }
+    return badNode;
+}
+function findACommonWeight(rootNode) {
+    let aWeight = rootNode.children[0].trueWeight;
+    for (let i = 1; i < rootNode.children.length; i++) {
+        if (rootNode.children[i].trueWeight === aWeight) return aWeight;
+    }
+    return rootNode.children[1].trueWeight;
+}
+function calculateNodeWeight(node) {
+    let weight = +node.weight;
+    node.children.forEach(childNode => {
+        weight += +calculateNodeWeight(childNode);
+    });
+    node.trueWeight = weight;
+    return weight;
 }
 
+function updateArrayOfNodes(arrayOfNodes) {
+    for (let key in arrayOfNodes) {
+        let childrenNodes = [];
+        arrayOfNodes[key].children.forEach(node => {
+            arrayOfNodes[node]._root = arrayOfNodes[key];
+            childrenNodes.push(arrayOfNodes[node]);
+        });
+        arrayOfNodes[key].children = childrenNodes;
+    }
+    return arrayOfNodes;
+}
 function findRoot(arrayOfNodes) {
-  for (let key in arrayOfNodes) {
-    if (arrayOfNodes[key]._root === null) return arrayOfNodes[key];
-  }
+    for (let key in arrayOfNodes) {
+        if (arrayOfNodes[key]._root === null) return arrayOfNodes[key];
+    }
 }
